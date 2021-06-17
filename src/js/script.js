3cfaad89eb05390990e5c9e84d58b0648061a2a1
sftvov <63662363+sftvov@users.noreply.github.com>
2021-06-17 20:27:50 +0300

    disappearance search

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,21 @@
 @@include('webp.js');
 
-ahaha = function() {
-  document.querySelector('.header__search').classList.toggle('active');
+let searchField = document.querySelector('.header__search');
+let searchButton = document.querySelector('.header__line-before');
+
+searchAppearanceToggle = function() {
+  searchField.classList.toggle('active');
+  searchButton.classList.toggle('active');
 };
-document.querySelector('.header__line-before').addEventListener('click', ahaha);
+
+searchDisappearance = function(e) {
+  if (e.target !== searchField.children[0] && e.target !== searchButton) {
+    searchField.classList.remove('active');
+    searchButton.classList.remove('active');
+  }
+};
+
+document.addEventListener('click', searchDisappearance);
+searchButton.addEventListener('click', searchAppearanceToggle);
 
 
