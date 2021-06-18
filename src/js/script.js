027af496e05c453bc713611a537b6859e60d50b2
sftvov <SUrabota@mail.ru>
2021-06-18 14:48:32 +0300

    70% of burger template

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,35 @@
 @@include('webp.js');
 
+// ----------------------------------------------------------------------
+
 let searchField = document.querySelector('.header__search');
 let searchButton = document.querySelector('.header__line-before');
+let searchInput = searchField.children[0];
 
 searchAppearanceToggle = function() {
   searchField.classList.toggle('active');
   searchButton.classList.toggle('active');
+  if (searchField.classList.contains('active')) {
+    searchInput.focus();
+  }
 };
 
 searchDisappearance = function(e) {
-  if (e.target !== searchField.children[0] && e.target !== searchButton) {
+  if (e.target !== searchInput && e.target !== searchButton) {
     searchField.classList.remove('active');
     searchButton.classList.remove('active');
   }
 };
 
-document.addEventListener('click', searchDisappearance);
+document.addEventListener('mousedown', searchDisappearance);
 searchButton.addEventListener('click', searchAppearanceToggle);
 
+// ----------------------------------------------------------------------
 
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+
+burger.onclick = function () {
+	burger.classList.toggle('active');
+	menu.classList.toggle('active');
+};
