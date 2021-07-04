41c87c010b352e5d41c1bce19119bb1461fa6a31
sftvov <63662363+sftvov@users.noreply.github.com>
2021-07-04 18:22:59 +0300

    finished burger and search

--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,20 @@
 @@include('webp.js');
+@@include('tabs.js');
 
 // ----------------------------------------------------------------------
 
-let header = document.querySelector('.header');
-let menu = document.querySelector('.header__menu');
-let burger = document.querySelector('.header__burger');
+
 let searchField = document.querySelector('.header__search');
 let searchInput = searchField.children[0];
 let searchButton = document.querySelector('.header__line-before');
-let height = menu.parentNode.clientHeight + 10 + 'px';
 
-searchDisappearance = function(e) {
-  if (e.target !== searchInput && e.target !== searchButton) {
-    searchField.classList.remove('active');
-    searchButton.classList.remove('active');
-  }
-};
-
-
-window.onresize = function () {  
-	burger.classList.remove('active');
-	menu.classList.remove('active');
-  menu.classList.remove('_container');
-  header.insertBefore(searchField, header.children[2]);
-  menu.style.paddingTop = 0;
-  searchField.classList.add('_container');
-};
-
-
-burger.onclick = function () {
-	burger.classList.toggle('active');
-	menu.classList.toggle('active');
-  menu.classList.toggle('_container');
-  if (burger.classList.contains('active')) {
-    menu.style.paddingTop = height;
-    menu.insertBefore(searchField, menu.children[0]);
-    searchField.classList.remove('_container');
-    } else {
-    menu.style.paddingTop = 0;
-    header.insertBefore(searchField, header.children[2]);
-    searchField.classList.add('_container');
-  }
-};
+document.addEventListener('mousedown', checkSearch);
 
 searchButton.onclick = () => {
+  toggleSearch();
+}
+
+function toggleSearch() {
   searchField.classList.toggle('active');
   searchButton.classList.toggle('active');
   if (searchField.classList.contains('active')) {
@@ -51,9 +22,37 @@ searchButton.onclick = () => {
   }
 }
 
+function closeSearch() {
+  searchField.classList.remove('active');
+  searchButton.classList.remove('active');
+}
 
-document.addEventListener('mousedown', searchDisappearance);
+function checkSearch(e) {
+  if (e.target !== searchInput && e.target !== searchButton) closeSearch();
+}
 
+// ----------------------------------------------------------------------
+
+let menu = document.querySelector('.header__menu');
+let burger = document.querySelector('.header__burger');
+
+burger.onclick = () => {
+  toggleBurger ();
+}
+
+window.onresize = () => {
+  closeBurger();
+}
+
+function toggleBurger() {
+	burger.classList.toggle('active');
+	menu.classList.toggle('active');
+}
+
+function closeBurger() {
+	burger.classList.remove('active');
+	menu.classList.remove('active');
+}
 
 
 
